028ac1c757e9452c0dc137f2267bdbd839917a2e
Nisarg <kavinisarg1@gmail.com>
2023-06-12 19:26:45 +0530

    Update keys.jsx

--- src/app/keys.jsx
+++ src/app/keys.jsx
@@ -1,4 +1,4 @@
 // API Key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyCQRTy9jEQT4kCOLwVkE1qMefbryh7gEIA";
+export const API_KEY = "AIzaSyAjzK_9dM44dMZb9KF5OO_WuChv8mGOv1E";
 // Context Key- https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "5384ba5eed4f84496";
+export const CONTEXT_KEY = "14f3672e8270449cf";
